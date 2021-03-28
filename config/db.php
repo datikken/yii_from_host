42afc3745d8e16103f94151e73a671a0d5f1e43a
datikken <support@beget.ru>
2021-03-28 13:15:26 +0300

    initial

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=tikken23_wp',
+    'username' => 'tikken23_wp',
+    'password' => '23Tikken',
+    'charset' => 'utf8',
+];
